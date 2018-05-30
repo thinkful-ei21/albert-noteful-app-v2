24e02f569fba16cc977750c76bfc077dac4da159
Albert Sare <thesundaymailman@gmail.com>
2018-05-30 13:59:17 -0700

    finished queries.js in scratch; ready to wire it up to routes/note.js

--- scratch/queries.js
+++ scratch/queries.js
@@ -2,7 +2,9 @@
 
 const knex = require('../knex');
 
-let searchTerm = 'gaga';
+
+// GET endpoint api/notes for search with/without searchTerm
+let searchTerm = null;
 knex
   .select('notes.id', 'title', 'content')
   .from('notes')
@@ -18,3 +20,61 @@ knex
   .catch(err => {
     console.error(err);
   });
+
+// GET rquest to endpoint api/notes/:id for returning a note by referencing its id
+// let searchId = 1002;
+// knex
+//   .select('notes.id', 'title', 'content')
+//   .from('notes')
+//   .modify(queryBuilder => {
+//     queryBuilder.where('notes.id', `${searchId}`);
+//   })
+//   .then(([results]) => {
+//     console.log(JSON.stringify(results, null, 2));
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+
+// PUT request to endpoint api/notes/:id for updating notes by id
+// let searchId = 1008;
+// let updateObject = {title: 'test title ultimate', content: 'ultimate test content'};
+// knex('notes')
+//   .where('notes.id', `${searchId}`)
+//   .update({title: updateObject.title, content: updateObject.content})
+//   .returning(['notes.id', 'title', 'content'])
+//   .then(([results]) => {
+//     console.log(JSON.stringify(results, null, 2));
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+// POST resquest to endpoint api/notes for adding a new note
+// let updateObject = {title: 'some title', content: 'some content'};
+// knex('notes')
+//   .insert({
+//     title: updateObject.title,
+//     content: updateObject.content
+//   })
+//   .returning(['notes.id', 'title', 'content'])
+//   .then(([results]) => {
+//     console.log(JSON.stringify(results, null, 2));
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+
+// DELETE request to endpoint api/notes for deleting a note by id
+// let searchId = 1009;
+// knex('notes')
+//   .where('notes.id', `${searchId}`)
+//   .del()
+//   .then(results => {
+//     console.log(results);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });

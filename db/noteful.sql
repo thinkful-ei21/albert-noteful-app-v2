0927af35bf0306538121e213614f8a7f8dbc281a
Albert Sare <thesundaymailman@gmail.com>
2018-06-01 09:05:58 -0700

    updated noteful.sql with tables 'tags' and 'notes_tags'

--- db/noteful.sql
+++ db/noteful.sql
@@ -5,6 +5,8 @@
 
 -- drops all tables first, if any
 -- tables being referenced will be dropped last
+DROP TABLE IF EXISTS notes_tags;
+DROP TABLE IF EXISTS tags;
 DROP TABLE IF EXISTS notes;
 DROP TABLE IF EXISTS folders;
 
@@ -13,7 +15,7 @@ DROP TABLE IF EXISTS folders;
 -- create folders table, alter start sequence, and populate sample data
 CREATE TABLE folders (
   id serial PRIMARY KEY,
-  name text NOT NULL
+  name text UNIQUE NOT NULL
 );
 
 ALTER SEQUENCE folders_id_seq RESTART WITH 100;
@@ -22,7 +24,8 @@ INSERT INTO folders (name) VALUES
 ('Archive'),
 ('Drafts'),
 ('Personal'),
-('Work');
+('Work')
+RETURNING id, name;
 
 
 
@@ -61,7 +64,7 @@ INSERT INTO notes (title, content, folder_id) VALUES
   (
     'The most incredible article about cats you''ll ever read',
     'Lorem ipsum dolor sit amet, boring consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    101
+    NULL
   ),
   (
     '10 ways cats can help you live to 100',
@@ -87,4 +90,46 @@ INSERT INTO notes (title, content, folder_id) VALUES
     'Why you should forget everything you learned about cats',
     'Posuere sollicitudin aliquam ultrices sagittis orci a. Feugiat sed lectus vestibulum mattis ullamcorper velit. Odio pellentesque diam volutpat commodo sed egestas egestas fringilla. Velit egestas dui id ornare arcu odio. Molestie at elementum eu facilisis sed odio morbi. Tempor nec feugiat nisl pretium. At tempor commodo ullamcorper a lacus. Egestas dui id ornare arcu odio. Id cursus metus aliquam eleifend. Vitae sapien pellentesque habitant morbi tristique. Dis parturient montes nascetur ridiculus. Egestas egestas fringilla phasellus faucibus scelerisque eleifend. Aliquam faucibus purus in massa tempor nec feugiat nisl.',
     101
-  );
+  )
+  RETURNING id, title;
+
+
+
+-- create tags table, alter start sequence, and populate sample data
+CREATE TABLE tags (
+  id serial PRIMARY KEY,
+  name text UNIQUE NOT NULL
+);
+
+ALTER SEQUENCE tags_id_seq RESTART WITH 100;
+
+INSERT INTO tags (name) VALUES
+('Funny'),
+('WTF'),
+('NSFW')
+RETURNING id, name;
+
+
+
+-- create notes_tags table, this is the many-to-many relationship between notes and tags table
+CREATE TABLE notes_tags (
+  note_id integer NOT NULL REFERENCES notes ON DELETE CASCADE,
+  tag_id integer NOT NULL REFERENCES tags ON DELETE CASCADE
+);
+
+INSERT INTO notes_tags (note_id, tag_id) VALUES
+(1000, 101),
+(1001, 100), (1001, 101), (1001, 102),
+(1002, 101), (1002, 102),
+(1003, 100), (1003, 101), (1003, 102),
+(1005, 100),
+(1007, 100), (1007, 102),
+(1008, 102)
+RETURNING note_id, tag_id;
+
+
+-- sample queries
+SELECT title, tags.name, folders.name FROM notes
+LEFT JOIN folders ON notes.folder_id = folders.id
+LEFT JOIN notes_tags ON notes.id = notes_tags.note_id
+LEFT JOIN tags ON notes_tags.tag_id = tags.id;
